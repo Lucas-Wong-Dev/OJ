0c521478175fc48d0de3116312055a480a0ae169
vpakati <34790891+vpakati@users.noreply.github.com>
2020-09-03 11:22:05 +0800

    9-3

--- sec/求前n项的和.cpp
+++ sec/求前n项的和.cpp
@@ -1,4 +1,5 @@
 // 求前n项的和1+2+…+n
+// AC
 /*
 问题描述
 求 1+2+3+...+n 的值。
@@ -27,9 +28,11 @@
 int main()
 {
     long long n, sum;
-    scanf("%I64d", &n);
+    // scanf("%I64d", &n); // AC：这种写法在蓝桥练习系统上能通过
+    scanf("%lld", &n); // AC
     sum = (n + 1) * n / 2;
-    printf("%I64d", sum); // 方便起见，竞赛时用cout输出sum
+    // printf("%I64d", sum); // AC
+    printf("%lld", sum); // AC
     return 0;
 }
 
@@ -39,20 +42,23 @@ int main()
 // {
 //     long long n;
 //     cin >> n;
-//     long long result;
-//     result = (1 + n) * n / 2;
-//     printf("%I64d", result);
+//     long long sum;
+//     sum = (1 + n) * n / 2;
+//     // printf("%I64d", sum); // AC
+//     // printf("%lld", sum); // AC
+//     cout << sum; // AC
 //     return 0;
 // }
 
-// 用 %u 打印 unsigned int 类型的变量
-// 用 %ld 打印 long 类型的变量
 
-// 对 long 类型
-// %lx 表示以十六进制格式打印长整数
-// %lo 表示以八进制格式打印长整数
+// 对 long 类型的变量
+// %ld 表示打印 long 类型的变量
+// %lx 表示以十六进制格式打印 long 类型的变量
+// %lo 表示以八进制格式打印 long 类型的变量
+
 // %lu 表示打印 unsigned long 类型的变量
+// %u  表示打印 unsigned int 类型的变量
 
-// 对 short 类型
+// 对 short 类型的变量
 // %hd 表示以十进制格式打印 short 类型的变量
 // %ho 表示以八进制格式打印 short 类型的变量
