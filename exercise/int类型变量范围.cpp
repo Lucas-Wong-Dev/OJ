09b0e3b39502230922a37fddb5fd60848ce5e7c4
vpakati <34790891+vpakati@users.noreply.github.com>
2020-09-10 22:46:55 +0800

    9-10

---
+++ exercise/int类型变量范围.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <limits.h>
+int main()
+{
+    printf("The value of INT_MAX is %i\n", INT_MAX);
+    printf("The value of INT_MIN is %i\n", INT_MIN);
+    printf("An int takes %d bytes\n", sizeof(int));
+    return 0;
+}
+
+
+// C 语言的 int 类型变量范围是多少？
+
+// c标准里面只定义了int的最小宽度，所以这个范围就完全依赖于编译器的实现了。请看c99的 5.2.4.2.1
+// Their implementation-defined values shall be equal or greater in magnitude (absolute value) to those shown, with the same sign.   
+// minimum value for an object of type int
+// INT_MIN -32767 // -(2^15 - 1)
+
+// https://www.zhihu.com/question/19580654
